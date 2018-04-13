e12bb7213cd3f489ae710b19d7c46a846061b5f8
nguyen <sonnc@deha-soft.com>
2018-04-13 20:56:59 +0700

    add profile site

--- SEProject/src/Model/User.java
+++ SEProject/src/Model/User.java
@@ -1,9 +1,11 @@
 package Model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import jdk.nashorn.api.scripting.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import Helper.Convertor;
+import java.sql.*;
 
 public class User {
     private String email;
@@ -99,4 +101,118 @@ public class User {
             }//end finally try
         }//end try
     }
+
+    public String getProfile(int id) {
+        Connection conn = null;
+        Statement stmt = null;
+        JSONArray obj = null;
+        Convertor convertor = new Convertor();
+        boolean returnValue = false;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT email, user_name, phone_number, DOB from user_info WHERE userID=" + id;
+            ResultSet rs = stmt.executeQuery(sql);
+            obj=convertor.convertToJSON(rs);
+
+
+            stmt.close();
+            conn.close();
+        } catch (
+                SQLException se)
+
+        {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (
+                Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally
+
+        {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        JSObject result = null;
+
+        return obj.toString();
+    }
+
+    public boolean exist(int id) {
+        Connection conn = null;
+        Statement stmt = null;
+        boolean returnValue = false;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM user_info WHERE userID="+id;
+            ResultSet rs = stmt.executeQuery(sql);
+            if (!rs.next()) {
+                returnValue = false;
+            } else {
+                returnValue = true;
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (
+                SQLException se)
+
+        {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (
+                Exception e)
+
+        {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally
+
+        {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        return returnValue;
+    }
+
 }
