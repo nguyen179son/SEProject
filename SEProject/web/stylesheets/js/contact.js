81640483668e7fcbe893f92225b3bbfe2c80e326
HungPhann <phanhuyhung1707@gmail.com>
2018-05-16 16:54:34 +0700

    add Show Friend Profile to Contact Page

--- SEProject/web/stylesheets/js/contact.js
+++ SEProject/web/stylesheets/js/contact.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
                             var htmlText = "";
                             response["friend_list"].forEach(function (friend) {
                                 if (friend["favorite"]) {
-                                    htmlText += "<li class=\"left clearfix\" data-id='" + friend['userID'] + "'>\n" +
+                                    htmlText += "<li class=\"left clearfix contact-box contact-box contact-box\" data-id='" + friend['userID'] + "'>\n" +
                                         "                     <span class=\"chat-img pull-left\">\n" +
                                         "                     <img src=\"./image/profile.png\"\n" +
                                         "                          alt=\"User Avatar\" class=\"img-circle\">\n" +
@@ -51,7 +51,7 @@ $(document).ready(function () {
                                         "                        </li>"
                                 }
                                 else {
-                                    htmlText += "<li class=\"left clearfix\" data-id='" + friend['userID'] + "'>\n" +
+                                    htmlText += "<li class=\"left clearfix contact-box contact-box contact-box\" data-id='" + friend['userID'] + "'>\n" +
                                         "                     <span class=\"chat-img pull-left\">\n" +
                                         "                     <img src=\"./image/profile.png\"\n" +
                                         "                          alt=\"User Avatar\" class=\"img-circle\">\n" +
@@ -132,7 +132,7 @@ $(document).ready(function () {
                             console.log(response);
                             response["friend_list"].forEach(function (friend) {
                                 if (friend["favorite"]) {
-                                    htmlText += "<li class=\"left clearfix\" data-id='" + friend['userID'] + "'>\n" +
+                                    htmlText += "<li class=\"left clearfix contact-box contact-box contact-box\" data-id='" + friend['userID'] + "'>\n" +
                                         "                     <span class=\"chat-img pull-left\">\n" +
                                         "                     <img src=\"./image/profile.png\"\n" +
                                         "                          alt=\"User Avatar\" class=\"img-circle\">\n" +
@@ -150,7 +150,7 @@ $(document).ready(function () {
                                         "                        </li>"
                                 }
                                 else {
-                                    htmlText += "<li class=\"left clearfix\" data-id='" + friend['userID'] + "'>\n" +
+                                    htmlText += "<li class=\"left clearfix contact-box contact-box contact-box\" data-id='" + friend['userID'] + "'>\n" +
                                         "                     <span class=\"chat-img pull-left\">\n" +
                                         "                     <img src=\"./image/profile.png\"\n" +
                                         "                          alt=\"User Avatar\" class=\"img-circle\">\n" +
@@ -176,13 +176,44 @@ $(document).ready(function () {
                         }
                     }
                     else {
-                         window.location = "/login";
+                        window.location = "/login";
                     }
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                     alert("Error");
                 }
 
+            });
+        };
+
+        contact.getFriendProfile = function (id) {
+            $.ajax({
+                url: "/get-friend-profile",
+                type: "post",
+                data: {
+                    token: window.localStorage.getItem("token"),
+                    friend_id: id
+                },
+                success: function (response) {
+                    console.log(response);
+                    if (response["verify_token"]) {
+                        if (response["success"]) {
+                            $("#profile-div").show();
+                            $("#nick-name").html(response["user_name"]);
+                            $("#email").html(response["email"]);
+                            $("#phone").html(response["phone_number"]==null?"":response["phone_number"]);
+                            $("#dob").html(response["DOB"]==null?"":response["DOB"]);
+                        }
+                        else {
+                            alert("INTERNAL ERROR");
+                        }
+                    } else {
+                        window.location = "/login";
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert("Error");
+                }
             });
         };
         return contact;
@@ -197,6 +228,10 @@ $(document).ready(function () {
 
         $("body").on("click", "#searchFriends", function () {
             Contact.searchFriends();
+        })
+
+        $("body").on("click", ".contact-box", function () {
+            Contact.getFriendProfile($(this).data("id"));
         })
 
     });
